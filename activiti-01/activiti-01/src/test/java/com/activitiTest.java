66067a9a851f596a55f9cf18d376d86d9350f4f5
dingzixuan <584321579@qq.com>
2022-05-18 23:06:25 +0800

    xiugai

--- activiti-01/activiti-01/src/test/java/com/activitiTest.java
+++ activiti-01/activiti-01/src/test/java/com/activitiTest.java
@@ -1,9 +1,11 @@
 package com;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngineConfiguration;
-import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.*;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.task.TaskQuery;
 import org.junit.Test;
+
+import java.util.List;
 
 public class activitiTest {
 
@@ -21,4 +23,30 @@ public class activitiTest {
 //        ProcessEngine processEngine = config.buildProcessEngine();
 //        System.out.println(processEngine);
 //    }
+
+    @Test
+    public void findTaskUserList(){
+        String userName = "zhangsan";
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        TaskService taskService = processEngine.getTaskService();
+        TaskQuery holiday = taskService.createTaskQuery().processDefinitionKey("holiday");
+        List<Task> tasks = holiday.list();
+        for (Task task : tasks) {
+            System.out.println("实例id:"+task.getProcessInstanceId());
+            System.out.println("任务id:"+task.getId());
+            System.out.println("任务名称:"+task.getName());
+            System.out.println("任务负责人"+task.getAssignee());
+        }
+    }
+
+    /**
+     * 删除实例
+     */
+    @Test
+    public void deleteTask(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        RepositoryService repositoryService = processEngine.getRepositoryService();
+        repositoryService.deleteDeployment("2501");
+
+    }
 }
